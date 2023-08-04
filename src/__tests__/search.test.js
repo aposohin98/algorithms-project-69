4d129109c9b380844eb6f83b9ed5623094531c44
Posokhin Alexander <126677779+aposohin98@users.noreply.github.com>
2023-08-04 19:39:46 +0500

    Выполнил шаг 3

--- src/__tests__/search.test.js
+++ src/__tests__/search.test.js
@@ -1,19 +1,19 @@
 import search from '../index.js';
 
-describe('Тестируем четкий поиск', () => {
-  it('Тест кейс #1', () => {
+describe('Тестируем функцию поиска', () => {
+  it('Возвращает пустой массив если не было найдено не одного результата', () => {
+    const docs = [];
+
+    expect(search(docs, 'shoot')).toEqual([]);
+  });
+
+  it('Ранжирует результаты', () => {
     const docs = [
       { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" },
       { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." },
       { id: 'doc3', text: "I'm your shooter." },
     ];
 
-    expect(search(docs, 'shoot')).toEqual(['doc1', 'doc2']);
-  });
-
-  it('Тест кейс #2', () => {
-    const docs = [];
-
-    expect(search(docs, 'shoot')).toEqual([]);
+    expect(search(docs, 'shoot')).toEqual(['doc2', 'doc1']);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,19 +1,47 @@
 import removeNonWordCharachers from './utils/removeNonWordCharachers.js';
 
-const search = (docs, searchWord) => docs.reduce((acc, { id, text }) => {
+const getSearchWordCount = (text, searchWord) => {
   const words = text.split(' ');
+
+  let count = 0;
 
   for (let i = 0; i < words.length; i += 1) {
     const word = words[i];
     const normalizedWord = removeNonWordCharachers(word);
 
     if (normalizedWord === searchWord) {
-      acc.push(id);
-      break;
+      count += 1;
     }
   }
 
-  return acc;
-}, []);
+  return count;
+};
+
+const search = (docs, searchWord) => {
+  const result = [];
+
+  let lastCount = -1;
+
+  for (let i = 0; i < docs.length; i += 1) {
+    const { id, text } = docs[i];
+
+    const count = getSearchWordCount(text, searchWord);
+
+    if (count === 0) {
+      // eslint-disable-next-line no-continue
+      continue;
+    }
+
+    if (count > lastCount) {
+      result.unshift(id);
+    } else {
+      result.push(id);
+    }
+
+    lastCount = count;
+  }
+
+  return result;
+};
 
 export default search;
